b688bb0e8a81c406a3b899dae312b2593be0065c
Marcos Ruiz Garcia <sobrenombre@gmail.com>
2018-11-21 13:38:06 +0100

    Deleted API entries deprecated

--- routes/index.js
+++ routes/index.js
@@ -22,17 +22,6 @@ router.post('/tsr', postTSR);
 
 router.post('/cert', processCSR);
 
-router.get('/example', getExample);
-
-router.get('/ocsp', function (req, res) {
-    res.send("Welcome to GET OCSP");
-});
-router.post('/ocsp', postOCSP);
-
-router.get('/ca.crl', getCRL);
-
-router.get('/revoke', revokeCert);
-
 function processCSR(req, res, next) {
     req.on('data', function (data) {
         fs.writeFileSync('file.csr', data);
@@ -65,58 +54,6 @@ function generateCSRReply(query, callback) {
         }
     });
 }
-
-
-/**
- * Not tested
- * @param req
- * @param res
- * @param next
- */
-function revokeCert(req, res, next) {
-    // Get cert
-    req.on('data', function (data) {
-        fs.writeFileSync('certToRevoke.pem', data);
-    });
-    req.on('end', function () {
-        // Revoke certificate
-        // openssl ca -config openssl.cnf -revoke ca/personalcert.pem -keyfile ca/private/cakey.pem -cert ca/cacert.pem
-        var cmd = `${ssl} ca -config ${config} -revoke certToRevoke.pem -keyfile ${cakey} -cert ${cacert}`;
-        const child = exec(cmd, (err, stdout, stderr) => {
-            if (err) return next(err);
-            res.status(HttpStatus.OK).send();
-        });
-    });
-}
-
-function getCRL(req, res, next) {
-    // Update CRL
-    // openssl ca -config openssl.cnf -gencrl -out ca/crl/ca.crl.pem
-    // openssl crl -inform PEM -in ca/crl/ca.crl.pem -outform DER -out ca/crl/ca.crl
-    var cmd = `${ssl} ca -config ${config} -gencrl -out ${crlPem}`;
-    const child = exec(cmd, (err, stdout, stderr) => {
-        if (err) return next(err);
-        cmd = `${ssl} crl -inform PEM -in ${crlPem} -outform DER -out ${crl}`;
-        const child2 = exec(cmd, (err, stdout, stderr) => {
-            if (err) return next(err);
-            // Send CRL
-            res.status(200);
-            res.download(crl, 'ca.crl');
-        });
-    });
-}
-
-var serviceCallback = function (response) {
-    return function (err, obj) {
-        console.log(response);
-        if (err) {
-            response.send(500);
-        } else {
-            response.send(obj);
-        }
-    }
-};
-
 /**
  * Source: https://stackoverflow.com/questions/33881316/node-js-signing-and-verifying-log-files-digitally-using-openssl
  * @param logfile
@@ -165,19 +102,6 @@ function generateTSReply(query, callback) {
 }
 
 /**
- *  GET /example
- *  Download a file .tsq
- */
-function getExample(req, res) {
-    // Creata a .tsq and send it
-    generateTSQuery('descarga.pdf', function (err, result) {
-        res.header('Content-Type', 'application/timestamp-reply');
-        res.header('Content-Disposition', 'attachment; filename=descarga.tsq');
-        res.download(result, 'descarga.tsq');
-    });
-}
-
-/**
  * POST TSR
  * Pre: receives a request using Time-Stamp Protocol (RFC3161)
  * Post: response with a binary file
@@ -208,23 +132,16 @@ function postTSR(req, res, next) {
     }
 }
 
-/**
- * check this with: openssl ocsp
- */
-function postOCSP(req, res) {
-    if (req.header('content-type') != 'application/ocsp-request') {
-        res.status(HttpStatus.BAD_REQUEST).send("Bad Request");
-    } else {
-        req.on('data', function (data) {
-            fs.writeFileSync('file.ocsp', data);
-            console.log(data);
-        });
-        req.on('end', function () {
-            console.log("ocsp");
-            res.header('content-type', 'application/ocsp-response');
-            res.status(200).send("BLA");
-        });
+var serviceCallback = function (response) {
+    return function (err, obj) {
+        console.log(response);
+        if (err) {
+            response.send(500);
+        } else {
+            response.send(obj);
+        }
     }
-}
+};
+
 
 module.exports = router;
